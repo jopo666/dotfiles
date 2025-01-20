5cfe2a5433ee42d495288e27ecb2bf5388f5c9f7
joona <jopo@thinkpad.home>
2025-01-20 08:10:27 +0200

    gruvbox best

--- config/nvim/lua/plugins/colorschemes.lua
+++ config/nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,12 @@
 return {
 	{
 		"sainnhe/gruvbox-material",
+		priority = 1000,
 		config = function()
 			vim.g.gruvbox_material_background = "hard"
 			vim.g.gruvbox_material_better_performance = true
 			vim.g.gruvbox_material_disable_italic_comment = false
+			vim.cmd([[ colorscheme gruvbox-material ]])
 		end,
 	},
 }
