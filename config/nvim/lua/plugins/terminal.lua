e22f46f2b7db0c2e05bbc718cbd337915b70d0e4
joona <jopo@thinkpad.home>
2025-02-01 10:32:44 +0200

    update toggleterm

--- config/nvim/lua/plugins/terminal.lua
+++ config/nvim/lua/plugins/terminal.lua
@@ -2,17 +2,28 @@ return {
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
-		config = function()
-			require("toggleterm").setup({
-				size = 20,
-				direction = "float",
-				float_opts = {
-					border = "single",
-				},
+		opts = {
+			persist_mode = false,
+			size = 20,
+			direction = "float",
+			float_opts = {
+				border = "single",
+			},
+		},
+		config = function(_, opts)
+			require("toggleterm").setup(opts)
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "toggleterm",
+				callback = function()
+					vim.opt_local.cursorline = false
+					vim.opt_local.laststatus = 0
+					vim.opt_local.signcolumn = "yes:1"
+				end,
 			})
 			vim.cmd([[
-				command Z w | qa
+				command Z wa | qa
 				cabbrev wqa Z
+
 			]])
 		end,
 		keys = {
